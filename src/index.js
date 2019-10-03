a1625e91470e70b9ffc3cca88c8c6d0316b22bb5
Марина Пискуненко <C:\Users\Asus\AppData\Roaming\The Bat!\Mail>
2019-10-03 19:09:22 +0300

    init

--- src/index.js
+++ src/index.js
@@ -39,6 +39,32 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    var arr = [];
+  var result = [];
+  while (expr.length) {
+    arr.push(expr.substring(0, 10));
+    expr = expr.substring(10);
+  }
+  arr.map(i => {
+    var mas = [];
+
+    i = +i;
+    i = i.toString()
+    for (let n = 0; n < i.length; n++) {
+      if (i[n] + i[n + 1] == "10") { mas.push(".") } else if (i == 'NaN') {
+        result.push(" ");
+        break
+      } else { mas.push('-') }
+      n++;
+    }
+    mas = mas.join('')
+    if (mas in MORSE_TABLE) {
+      result.push(MORSE_TABLE[mas]);
+    }
+
+  })
+
+  return result.join('');
 }
 
 module.exports = {
